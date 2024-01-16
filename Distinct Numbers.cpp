bc226905e63e803e57ecfdaa2001f748e36ec18b
Saahir <103967382+saahirvazirani@users.noreply.github.com>
2024-01-15 21:14:22 -0500

    Add files via upload

---
+++ Distinct Numbers.cpp
@@ -0,0 +1,15 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	set<int> s;
+	for (int i =0; i <n; i++) {
+		int a; 
+		cin >> a;
+		s.insert(a);
+	}
+	cout << s.size() << endl;
+}
